772529f8a4f4015357305f97bb4aeda79c1afecf
Samuel Fleet <sfleet@ucsd.edu>
2021-02-25 14:51:05 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'zIOd8y1RK1NIyhbn3Hd6TndmraohVQei';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.87877975751717, -117.23591115434338],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+
+L.marker([32.88, -117.236]).addTo(map);
 }
